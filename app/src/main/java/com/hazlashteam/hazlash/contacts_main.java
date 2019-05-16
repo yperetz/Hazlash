31c031648ef341f9b7e274f82ab58fc711d49ace
miri.aframov <miri.aframov@mail.huji.ac.il>
2019-05-16 22:06:02 +0300

    latest version Miri

--- app/src/main/java/com/hazlashteam/hazlash/contacts_main.java
+++ app/src/main/java/com/hazlashteam/hazlash/contacts_main.java
@@ -2,12 +2,27 @@ package com.hazlashteam.hazlash;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ScrollView;
+
+import java.lang.reflect.Array;
+import java.util.Map;
 
 public class contacts_main extends AppCompatActivity {
+
+    String[] contactsList = new String[10];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts_main);
+        RecyclerView myContactList = (RecyclerView) findViewById(R.id.myContactList);
     }
+
+    private void createContactList(){
+        contactsList = new String[]{"Mom","Dad","Sister","Boyfriend","Work","School"};
+
+
+    }
+
 }
